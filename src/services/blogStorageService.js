95fdc9d4f5e3f6bd60b55e3bcf7b770582487398
Cuong Nguyen <cuong.ngman@gmail.com>
2023-12-20 18:12:58 +0700

    implement blob storage service, write webhooks data to storage blob

---
+++ src/services/blogStorageService.js
@@ -0,0 +1,45 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { createTableServiceWithSas } from "azure-storage";
+
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+export class BlobStorageService {
+  #client;
+  #logger;
+
+  constructor(options) {
+    const { accountName, logger } = this.validate(options);
+    const blobURL = `https://${accountName}.blob.core.windows.net`;
+    this.#client = new BlobServiceClient(blobURL, new DefaultAzureCredential());
+    this.#logger = logger;
+  }
+
+  validate(options) {
+    if (!options.accountName) {
+      throw new Error("Azure Storage Account Name not found");
+    }
+
+    return options;
+  }
+
+  async upload(containerName, blobName, data) {
+    try {
+      this.logger.log("creating log...");
+      const container = this.client.getContainerClient(containerName);
+      const blockBlobClient = container.getBlockBlobClient(blobName);
+      const response = await blockBlobClient.upload(data, data.length);
+      return [true, response.requestId];
+    } catch (error) {
+      this.logger.error(`Error creating log: ${containerName}/${blobName}::${data}`, error);
+      return [false, null];
+    }
+  }
+
+  get client() {
+    return this.#client;
+  }
+
+  get logger() {
+    return this.#logger;
+  }
+}
